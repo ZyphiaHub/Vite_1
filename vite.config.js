0822f4e1e228f6e36e556ea96622fccfae525adb
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-10 20:36:32 +0100

    Changes to be committed: 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/myproject/',
+  base: '/Vite_1/',
   build: {
       outDir: 'docs',
       emptyOutDir: true
